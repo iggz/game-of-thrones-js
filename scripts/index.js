f89e514bccbb9f12d6c7682c8fccb13b2aae251a
igor popenov <ipopenov@protonmail.com>
2019-05-09 15:44:51 -0400

    First 5 queries are complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,91 @@
-console.log(`There are ${characters.length} characters in the array.`);
+// console.log(`There are ${characters.length} characters in the array.`);
 
 console.log(characters[0]);
 
-// characters.forEach(function(character){
-//     console.log(`Charactre name: ${character.name}`);
-// })
+total = 0;
+characters.forEach(function(character){
+    total ++
+})
+console.log(`Total number of characters: ${total}`);
+
+//////////////////////////////////////////////////////////////
+//How many character names start with "A"?////////////////////
+//////////////////////////////////////////////////////////////
+
+let aCharactersArray = [];
+
+characters.forEach(function(character){
+    if (character.name[0] == "A") {
+        aCharactersArray.push(character.name);
+    }
+});
+
+console.log(`There are ${aCharactersArray.length} characters who's name starts with "A"`);
+
+//////////////////////////////////////////////////////////////
+//How many character names start with "Z"?////////////////////
+//////////////////////////////////////////////////////////////
+
+let zCharacterCounter = 0;
+
+characters.forEach(function(character){
+    if (character.name[0] == "Z"){
+        zCharacterCounter += 1;
+    }
+})
+
+console.log(`There are ${zCharacterCounter} character names that start with Z.`)
+
+//////////////////////////////////////////////////////////////
+//How many character are dead?////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+let deadCharacters = [];
+
+characters.forEach(function(character){
+    if (character.died[0] != ""){
+        deadCharacters.push(character.died);
+    }
+})
+
+console.log(`There are ${deadCharacters.length} dead characters`);
+
+//////////////////////////////////////////////////////////////
+//Who has the most titles?////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+let mostTitles = 0;
+let characterWithMostTitles = "";
+
+characters.forEach(function(character){
+    if (character.titles.length > mostTitles){
+        mostTitles = character.titles.length;
+        characterWithMostTitles = character.name;
+
+        // mostTitles = character.name[0];
+    }
+})
+console.log(`Largest amount of titles: ${mostTitles}`)
+console.log(`Character With Most Titles: ${characterWithMostTitles}`);
+
+
+//////////////////////////////////////////////////////////////
+//How many Are Valyrian?//////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+let valyrianCount = 0;
+let valyrianArray = [];
+
+characters.forEach(function(character){
+    if (character.culture = "Valyrian"){
+        valyrianArray.push(character.name)
+        valyrianCount += 1;
+    }
+})
+
+console.log(valyrianCount);
+console.log(valyrianArray);
+//////////////////////////////////////////////////////////////
+//What actor play "Hot Pie"?
+//////////////////////////////////////////////////////////////
+
