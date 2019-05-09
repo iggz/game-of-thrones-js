cc848a303edca1814f9d2d5e9cfabcfe6d50412e
igor popenov <ipopenov@protonmail.com>
2019-05-09 14:20:12 -0400

    First commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
 console.log(`There are ${characters.length} characters in the array.`);
+
+console.log(characters[0]);
+
+// characters.forEach(function(character){
+//     console.log(`Charactre name: ${character.name}`);
+// })
